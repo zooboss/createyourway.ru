c3115fec051f4f5ade370752c2fc0dab9fda5ba7
zooboss <sandrozz@yandex.ru>
2016-04-25 19:38:41 +0300

    Executed main layout with minor interactive

---
+++ scripts/script-main.js
@@ -0,0 +1,23 @@
+$(document).ready(function()
+{
+    $(document).on('mouseenter', '.item', function()
+    {
+        $(this).css("background-color", "black");        
+    });
+    $(document).on('mouseout', '.item', function()
+    {
+        $(this).css("background-color", "deepskyblue");        
+    });
+    
+            
+    
+            
+        
+                             
+    
+
+    
+    
+    
+    
+});
--- veiws/mainpage.php
+++ veiws/mainpage.php
@@ -4,29 +4,31 @@
     <title>Create Your Way</title>
     <meta charset = "utf-8">
     <link rel = "stylesheet" type = "text/css" href = "css/mainstyle.css">
-    <script type = "text/javascript" src = "/libraries/jquery-1.12.3.min.js"></script>
-    <script type = "text/javascript" src = "/libraries/jquery-ui.min.js"></script>
+    <script type = "text/javascript" src = "libraries/jquery-1.12.3.min.js"></script>
+    <script type = "text/javascript" src = "libraries/jquery-ui.min.js"></script>
+    <script type = "text/javascript" src = "scripts/script-main.js"></script>
 </head>
     
 <body>
-    <header>
-        <div id = "logo"></div>
-    </header>
+    
     <main>
         <section>
+            <header>
+                <div class = "logo"></div>
+            </header>
             <div class = "container">
                 <div class = "container-top">
-                    <div class = "item sub_service"></div>
-                    <div class = "item sub_service"></div>
+                    <div class = "item sub-service"></div>
+                    <div class = "item sub-service"></div>
                 </div>
                 <div class = "container-center">
-                    <div class = "item sub_service"></div>
-                    <div class = "item main_service"></div>
-                    <div class = "item sub_service"></div>
+                    <div class = "item sub-service"></div>
+                    <div class = "item main-service"></div>
+                    <div class = "item sub-service"></div>
                 </div>
                 <div class = "container-bottom">
-                    <div class = "item sub_service"></div>
-                    <div class = "item sub_service"></div>
+                    <div class = "item sub-service"></div>
+                    <div class = "item sub-service"></div>
                 </div>
             </div>
         </section>
