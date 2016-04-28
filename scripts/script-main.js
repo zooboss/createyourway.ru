f3ff702bf71a7495ce11a5b2f622f3d668752f43
zooboss <sandrozz@yandex.ru>
2016-04-28 13:03:08 +0300

    Finally developed animated first page sitename and button

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -29,6 +29,29 @@ $(document).ready(function()
         $(this).removeClass("button-highlight");
         $(this).addClass("button-highlight-end");
     });
+    
+    
+    var clicks = 0;
+    $(".button").click(function()
+    {
+        if (clicks % 2 == 0)
+        {
+            $(".button").removeClass("click-end");
+            $(".button").addClass("click")
+            $(".sitename").removeClass("click-end");
+            $(".sitename").addClass("click");
+            
+        }
+        else
+        {
+            $(".button").removeClass("click");
+            $(".button").addClass("click-end");
+            $(".sitename").removeClass("click");
+            $(".sitename").addClass("click-end");
+        }
+    
+       clicks++;
+    });
      
     
 });
