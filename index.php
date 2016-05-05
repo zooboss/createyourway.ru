ec73fb00f2267acd9d677bbe22287056a8ba989d
zooboss <sandrozz@yandex.ru>
2016-05-05 22:23:07 +0300

    reworked if condition

--- index.php
+++ index.php
@@ -4,15 +4,13 @@
 
     //$link = db_connect();
     
-    
-    $action = isset($_GET['action']) ? $_GET['action'] : NULL;
-    echo $action;
-
-    if($action == 'email') {
-        sendmail($_POST['mail_name'], $_POST['mail_contact'], $_POST['mail_subject'], $_POST['mail_text']);
-        header("Location: index.php#anchor");
+    if (isset($_GET['action']))
+    {
+        if($_GET['action'] == 'email') {
+            sendmail($_POST['mail_name'], $_POST['mail_contact'], $_POST['mail_subject'], $_POST['mail_text']);
+            header("Location: index.php#anchor");
+        }
     }
-
 
     include(dirname(__FILE__)."/veiws/mainpage.php");
     
