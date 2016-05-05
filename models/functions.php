4c0ac7f8ed8c699d435b42fe0d9f825f5867f1f6
zooboss <sandrozz@yandex.ru>
2016-05-06 01:58:41 +0300

    function works but mail settings doesn't

--- models/functions.php
+++ models/functions.php
@@ -3,6 +3,7 @@ function sendmail($name, $email, $subject, $text)
 {
     $headers = "From:";
     mail("sandrozz@yandex.ru", $subject, $text, $headers);
+    echo $name . $email . $subject . $text;
     
 }
 
